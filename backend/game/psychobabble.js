c22f0c5b5e2e8dc438ae30b13f56a505f36ca022
Soedarsono <soedar.sg@gmail.com>
2017-06-02 00:05:32 +0800

    Added psychobabble logic for sentence builder and valid sentences

---
+++ backend/game/psychobabble.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function Psychobabble(generator) {
+  return {
+    builder: builder,
+    generator: generator,
+    valid: valid,
+  };
+}
+
+function builder(words) {
+  var build = function(words, results, currentWord) {
+    if (words.length === 0) {
+      if (currentWord) {
+        results.push(currentWord.root);
+      }
+      return results.join(' ');
+    }
+
+    var word = words.shift();
+    if (word.type === 'modifier') {
+      var compoundWord = { type: 'compound', root: '' };
+
+      if (word.root in currentWord) {
+        compoundWord.root = currentWord[word.root];
+      } else {
+        compoundWord.root = currentWord.root + word.root;
+      }
+
+      return build(words, results, compoundWord);
+    } else {
+      results.push(currentWord.root);
+      return build(words, results, word);
+    }
+  };
+
+  words = words.slice();
+  return build(words, [], words.shift());
+}
+
+function valid(words, inputWords) {
+  var wordsAllowed = function(words, inputWords) {
+    inputWords = new Set(inputWords.map(w => w.id));
+    var roundWords = new Set(words.map(w => w.id));
+
+    for (let w of inputWords) {
+      if (!roundWords.has(w)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var hasDuplicate = function(inputWords) {
+    var size = inputWords.length;
+    var setSize = new Set(inputWords.map(w => w.id)).size;
+
+    return size !== setSize;
+  };
+
+  return !hasDuplicate(inputWords) && wordsAllowed(words, inputWords);
+}
+
+exports = module.exports = Psychobabble;
