f8ef398b33b791c1173bb04324e54f1c55fb2e8a
Daryl Tan <daryltanwk@gmail.com>
2017-06-01 19:38:22 +0800

    base routing logic, added home and play components, bootstrap stuff

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from "app/home/home.component";
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
